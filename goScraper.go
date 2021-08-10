2dab676a176f667af48e5825626d7d1c187a4832
Lakshya <lakshya.gupta.1999@gmail.com>
2021-08-11 03:26:35 +0530

    complete working app

---
+++ goScraper.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	fName := "data.csv"
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatal("Could not create file, err :%q", err)
+		return
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("internshala.com"),
+	)
+
+	c.OnHTML(".internship_meta", func(e *colly.HTMLElement) {
+
+		writer.Write([]string{
+			e.ChildText("a"),
+			e.ChildText("span"),
+		})
+	})
+
+	for i := 0; i < 329; i++ {
+		fmt.Printf("Scraping Page : %d\n", i)
+
+		c.Visit("https://internshala.com/internships/page-" + strconv.Itoa(i))
+	}
+
+	log.Printf("Scraping Complete")
+	log.Println(c)
+}
